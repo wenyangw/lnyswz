df0e6c488035782cea3e6d46a17fcfed979920cb
Wenyang <Wenyang@Wangwy>
2017-06-19 14:48:21 +0800

    开始客诉管理

---
+++ src/lnyswz/jxc/bean/Ksd.java
@@ -0,0 +1,252 @@
+package lnyswz.jxc.bean;
+
+import java.util.Date;
+
+public class Ksd {
+	private String ksdlsh;
+	private String bmbh;
+	private String bmmc;
+	private Date createTime;
+	private Integer createId;
+	private String createName;
+	private String khbh;
+	private String khmc;
+	private String yc;
+	private String lxbm;
+	private String lxr;
+	private String zw;
+	private String dh;
+	private String gysbh;
+	private String gysmc;
+	private String zzlx;
+	private String kslx;
+	private String ksyy;
+	private String jbr;
+	private Date jbTime;
+	private String xcjg;
+	private String xcjbr;
+	private Date xcTime;
+	private String gysyj;
+	private String gysjbr;
+	private Date gysTime;
+	private String cljg;
+	private Date clTime;
+	private String status;
+	private Integer yclId;
+	private String yclName;
+	private Date yclTime;
+	private Integer hxId;
+	private String hxName;
+	private Date hxTime;
+	public String getKsdlsh() {
+		return ksdlsh;
+	}
+	public void setKsdlsh(String ksdlsh) {
+		this.ksdlsh = ksdlsh;
+	}
+	public String getBmbh() {
+		return bmbh;
+	}
+	public void setBmbh(String bmbh) {
+		this.bmbh = bmbh;
+	}
+	public String getBmmc() {
+		return bmmc;
+	}
+	public void setBmmc(String bmmc) {
+		this.bmmc = bmmc;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Integer getCreateId() {
+		return createId;
+	}
+	public void setCreateId(Integer createId) {
+		this.createId = createId;
+	}
+	public String getCreateName() {
+		return createName;
+	}
+	public void setCreateName(String createName) {
+		this.createName = createName;
+	}
+	public String getKhbh() {
+		return khbh;
+	}
+	public void setKhbh(String khbh) {
+		this.khbh = khbh;
+	}
+	public String getKhmc() {
+		return khmc;
+	}
+	public void setKhmc(String khmc) {
+		this.khmc = khmc;
+	}
+	public String getYc() {
+		return yc;
+	}
+	public void setYc(String yc) {
+		this.yc = yc;
+	}
+	public String getLxbm() {
+		return lxbm;
+	}
+	public void setLxbm(String lxbm) {
+		this.lxbm = lxbm;
+	}
+	public String getLxr() {
+		return lxr;
+	}
+	public void setLxr(String lxr) {
+		this.lxr = lxr;
+	}
+	public String getZw() {
+		return zw;
+	}
+	public void setZw(String zw) {
+		this.zw = zw;
+	}
+	public String getDh() {
+		return dh;
+	}
+	public void setDh(String dh) {
+		this.dh = dh;
+	}
+	public String getGysbh() {
+		return gysbh;
+	}
+	public void setGysbh(String gysbh) {
+		this.gysbh = gysbh;
+	}
+	public String getGysmc() {
+		return gysmc;
+	}
+	public void setGysmc(String gysmc) {
+		this.gysmc = gysmc;
+	}
+	public String getZzlx() {
+		return zzlx;
+	}
+	public void setZzlx(String zzlx) {
+		this.zzlx = zzlx;
+	}
+	public String getKslx() {
+		return kslx;
+	}
+	public void setKslx(String kslx) {
+		this.kslx = kslx;
+	}
+	public String getKsyy() {
+		return ksyy;
+	}
+	public void setKsyy(String ksyy) {
+		this.ksyy = ksyy;
+	}
+	public String getJbr() {
+		return jbr;
+	}
+	public void setJbr(String jbr) {
+		this.jbr = jbr;
+	}
+	public Date getJbTime() {
+		return jbTime;
+	}
+	public void setJbTime(Date jbTime) {
+		this.jbTime = jbTime;
+	}
+	public String getXcjg() {
+		return xcjg;
+	}
+	public void setXcjg(String xcjg) {
+		this.xcjg = xcjg;
+	}
+	public String getXcjbr() {
+		return xcjbr;
+	}
+	public void setXcjbr(String xcjbr) {
+		this.xcjbr = xcjbr;
+	}
+	public Date getXcTime() {
+		return xcTime;
+	}
+	public void setXcTime(Date xcTime) {
+		this.xcTime = xcTime;
+	}
+	public String getGysyj() {
+		return gysyj;
+	}
+	public void setGysyj(String gysyj) {
+		this.gysyj = gysyj;
+	}
+	public String getGysjbr() {
+		return gysjbr;
+	}
+	public void setGysjbr(String gysjbr) {
+		this.gysjbr = gysjbr;
+	}
+	public Date getGysTime() {
+		return gysTime;
+	}
+	public void setGysTime(Date gysTime) {
+		this.gysTime = gysTime;
+	}
+	public String getCljg() {
+		return cljg;
+	}
+	public void setCljg(String cljg) {
+		this.cljg = cljg;
+	}
+	public Date getClTime() {
+		return clTime;
+	}
+	public void setClTime(Date clTime) {
+		this.clTime = clTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Integer getYclId() {
+		return yclId;
+	}
+	public void setYclId(Integer yclId) {
+		this.yclId = yclId;
+	}
+	public String getYclName() {
+		return yclName;
+	}
+	public void setYclName(String yclName) {
+		this.yclName = yclName;
+	}
+	public Date getYclTime() {
+		return yclTime;
+	}
+	public void setYclTime(Date yclTime) {
+		this.yclTime = yclTime;
+	}
+	public Integer getHxId() {
+		return hxId;
+	}
+	public void setHxId(Integer hxId) {
+		this.hxId = hxId;
+	}
+	public String getHxName() {
+		return hxName;
+	}
+	public void setHxName(String hxName) {
+		this.hxName = hxName;
+	}
+	public Date getHxTime() {
+		return hxTime;
+	}
+	public void setHxTime(Date hxTime) {
+		this.hxTime = hxTime;
+	}
+	
+}
