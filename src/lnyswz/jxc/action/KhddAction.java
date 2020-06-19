d264800525fb16855d1f8b8044c6c6b2f83702e2
wenyangw <wenyangw@gmail.com>
2020-06-19 10:22:19 +0800

    优化客户订单

--- src/lnyswz/jxc/action/KhddAction.java
+++ src/lnyswz/jxc/action/KhddAction.java
@@ -5,11 +5,14 @@ import lnyswz.common.action.BaseAction;
 import lnyswz.common.bean.DataGrid;
 import lnyswz.common.bean.Json;
 import lnyswz.jxc.bean.Khdd;
+import lnyswz.jxc.bean.KhddDet;
 import lnyswz.jxc.bean.User;
 import lnyswz.jxc.service.KhddServiceI;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * 客户订单Action
@@ -56,8 +59,11 @@ public class KhddAction extends BaseAction implements ModelDriven<Khdd>{
 			Khdd k = khddService.cancelKhdd(khdd);
 			if(k != null){
                 j.setObj(k);
+                j.setMsg("取消客户订单成功！");
                 j.setSuccess(true);
-			}
+			} else {
+                j.setMsg("取消客户订单不成功！");
+            }
 		} catch (Exception e) {
 			j.setMsg("取消客户订单失败！");
 			e.printStackTrace();
@@ -136,7 +142,11 @@ public class KhddAction extends BaseAction implements ModelDriven<Khdd>{
 
 	public void getKhddDet(){
 	    Json j = new Json();
-	    j.setObj(khddService.getKhddDet(khdd));
+	    List<KhddDet> l = khddService.getKhddDet(khdd);
+	    if (l != null && l.size() > 0) {
+	    	j.setSuccess(true);
+			j.setObj(l);
+		}
 		writeJson(j);
 	}
 
